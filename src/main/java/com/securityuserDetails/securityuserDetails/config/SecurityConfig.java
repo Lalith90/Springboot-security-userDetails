9b1d4ab2252d5928b3a165ea87d7ba6965981c6d
Lositha <asakahatapitiya@gmail.com>
2019-02-02 20:35:56 +0530

    Password encoder was setted

--- src/main/java/com/securityuserDetails/securityuserDetails/config/SecurityConfig.java
+++ src/main/java/com/securityuserDetails/securityuserDetails/config/SecurityConfig.java
@@ -2,27 +2,32 @@ package com.securityuserDetails.securityuserDetails.config;
 
 import com.securityuserDetails.securityuserDetails.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true) // this need to add get method level security
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    // password encode set
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return  new BCryptPasswordEncoder();
+    }
 
     @Autowired
     private CustomUserDetailsService userDetailsService;
 
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService)
-        .passwordEncoder(getPasswordEncoder());
-
+        auth.userDetailsService(userDetailsService).passwordEncoder(encodePWD());
     }
 
     @Override
@@ -34,7 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .and().formLogin().permitAll();
     }
 
-    private PasswordEncoder getPasswordEncoder() {
+    /*private PasswordEncoder getPasswordEncoder() {
         return new PasswordEncoder() {
             @Override
             public String encode(CharSequence rawPassword) {
@@ -46,5 +51,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 return true;
             }
         };
+    }*/
+
+    @Bean
+    public BCryptPasswordEncoder encodePWD() {
+        return new BCryptPasswordEncoder();
     }
 }
